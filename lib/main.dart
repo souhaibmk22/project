293ad8bb4314b3adb4774f9f5a9e2e6064777ef5
souhaib <s.belmaksene@esi-sba.dz>
2024-02-04 22:55:04 +0100

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "TowPall",
+      title: "TowPaLL",
       home: SplashScreen(),
     );
   }
