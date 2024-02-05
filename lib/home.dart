678667c2c7173550db5ab012808788983290e356
Amine <mohamedaminebengherabi@gmail.com>
2024-02-06 00:56:36 +0100

    from phone signup page to full name page front end (version 1.0.0)

---
+++ lib/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'phone_sign_in.dart';
+
+class home extends StatefulWidget {
+  const home({super.key});
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            '.idea/assets/images/img.png',
+            fit: BoxFit.fill,
+          ),
+          Container(
+            child: Center(
+                child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+              ),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => phone_sign()));
+              },
+              child: Text(
+                'Sign in with Phone number',
+                style: GoogleFonts.poppins(textStyle: TextStyle(fontSize: 18)),
+              ),
+            )),
+          )
+        ],
+      ),
+    );
+  }
+}
